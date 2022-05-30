c78b34f2f91efbf4a7468d6dda1e64c210906785
Dima_SHMEL <55811573+DimaSHMEL@users.noreply.github.com>
2022-05-30 16:59:52 +0300

    Sign_in activity and page is done
    1. Layout for the sign in page was done
    2. Also for this page was made Activity which checks username and password
    3. Android Manifest was changed to make register page first one

---
+++ SMC_orgonaizer_app/app/src/main/java/com/example/smc_orgonaizer_app/SignInActivity.java
@@ -0,0 +1,66 @@
+package com.example.smc_orgonaizer_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private Button enterBtn;
+    private EditText PasswordView;
+    private EditText UsernameView;
+    private TextView invalidUsernameView;
+    private TextView invalidPasswordView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Отлючение имени приложения
+        getSupportActionBar().hide();
+        setContentView(R.layout.sing_in_page);
+        //Получение view полльзователя и пароля
+        UsernameView = findViewById(R.id.sign_in_page_username);
+        PasswordView = findViewById(R.id.sign_in_page_password);
+        enterBtn = findViewById(R.id.sign_in_page_sign_in_btn);
+        invalidUsernameView = findViewById(R.id.sign_in_invalid_username);
+        invalidPasswordView = findViewById(R.id.sign_in_invalid_password);
+    }
+    String username = "1";
+    String password = "1";
+    private boolean CheckUser(String usernameIn)
+    {
+        return username.equals(usernameIn);
+    }
+    private boolean CheckPassword(String passwordIn)
+    {
+        return password.equals(passwordIn);
+    }
+
+    //Вход по нажатию кнопки
+
+    public void enterAccount(View view)
+    {
+        String usernameIn = UsernameView.getText().toString();
+        String passwordIn = PasswordView.getText().toString();
+        if(CheckPassword(passwordIn) && CheckUser(usernameIn))
+        {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+        else if(!CheckPassword(usernameIn))
+        {
+            invalidPasswordView.setVisibility(View.VISIBLE);
+            invalidUsernameView.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            invalidPasswordView.setVisibility(View.VISIBLE);
+        }
+    }
+}
